8ae4994cef4c20a4c74b583398b50c5d697f6f9e
Ibrar-Haidar <106898965+Ibrar-Haidar@users.noreply.github.com>
2025-06-01 19:29:27 +0500

    Updated Sign up and login form v1

--- apen-dash/app/sign-up/page.tsx
+++ apen-dash/app/sign-up/page.tsx
@@ -1,6 +1,6 @@
-import { GalleryVerticalEnd } from "lucide-react"
+import { GalleryVerticalEnd } from "lucide-react";
 
-import { LoginForm } from "@/components/login-form"
+import { SignUpForm } from "@/components/sign-up-form";
 
 export default function LoginPage() {
   return (
@@ -8,25 +8,27 @@ export default function LoginPage() {
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
+            <div className=" flex items-center justify-center">
+              <img
+                src="/logo-dark.svg"
+                alt="Image"
+              />
             </div>
-            Acme Inc.
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
-            <LoginForm />
+            <SignUpForm />
           </div>
-        </div>
+        </div>  
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.svg"
+          src="/fantasy-style-clouds-with-city.jpg"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.8] dark:grayscale"
         />
       </div>
     </div>
-  )
+  );
 }
